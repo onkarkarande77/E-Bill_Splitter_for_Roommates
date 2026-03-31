d7ace3c905ab933f466192aaed4f9ec33175e5f6
onkarkarande77 <karandeonkar05@gmail.com>
2026-03-31 18:41:09 +0530

    Update Main.java

--- E-Bill Splitter for Roommates/E_Bill-Spilleter/src/com/ebillsplitter/main/Main.java
+++ E-Bill Splitter for Roommates/E_Bill-Spilleter/src/com/ebillsplitter/main/Main.java
@@ -4,6 +4,6 @@ import com.ebillsplitter.ui.LoginFrame;
 
 public class Main {
     public static void main(String[] args) {
-         new LoginFrame().setVisible(true);
+          new LoginFrame().setVisible(true);
     }
 }
